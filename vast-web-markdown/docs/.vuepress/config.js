d4f7a8043813853a4a7ca1e0194e451036baab2c
zhuangabiao <hzwyingyan@126.com>
2021-02-01 23:38:20 +0800

    添加logo

--- vast-web-markdown/docs/.vuepress/config.js
+++ vast-web-markdown/docs/.vuepress/config.js
@@ -1,14 +1,17 @@
 module.exports = {
+    host: 'localhost',  //ip
+    port: '8099',
     title: 'ReadMark',
     description: '我的个人网站',
-    head: [ // 注入到当前页面的 HTML <head> 中的标签
-        ['link', { rel: 'icon', href: '/logo.jpg' }], // 增加一个自定义的 favicon(网页标签的图标)
-    ],
     base: '/', // 这是部署到github相关的配置
+    head: [ // 注入到当前页面的 HTML <head> 中的标签
+        ['link', { rel: 'icon', href: '/images/logo.jpg' }], // 增加一个自定义的 favicon(网页标签的图标)
+    ],
     markdown: {
         lineNumbers: false // 代码块显示行号
     },
     themeConfig: {
+        logo: '/images/logo.jpg',
         nav:[ // 导航栏配置
             {text: '前端基础', link: '/accumulate/' },
             {text: '算法题库', link: '/algorithm/'},
@@ -21,17 +24,48 @@ module.exports = {
             }
         ],
         // 侧边栏配置
-        sidebar: {
-          '/home/':{
-              title: '',
-              collapsable: false,
-              children: [
-                  { title: '生活测试01', path: '/life/life01' },
-                  { title: '生活测试02', path: '/life/life02' },
-                  { title: '生活测试03', path: '/life/life03' },
-              ]
-          }
-        },
+        sidebar: [
+            {
+                title: 'items01',
+                collapsable: false,
+                children: [
+                    { title: 'items01', path:'/dev_manage/'},
+                    { title: 'items02', path:'/dev_manage/aaa'}
+                ]
+            },
+            {
+                title: 'items02',
+                collapsable: false,
+                children: [
+                    { title: 'items01', path:'/literature/aaa'},
+                    { title: 'items02', path:'/literature/bbbb'}
+                ]
+            },
+            {
+                title: 'items03-1',
+                collapsable: false,
+                children: [
+                    { title: 'items01', path:'/guide/'},
+                    { title: 'items01', path: '/guide/bbbb' }
+                ]
+            },
+                {
+                    title: 'items03-2',
+                    collapsable: false,
+                    children: [
+                        { title: 'items01', path:'/guide/aaaa/'},
+                        { title: 'items01', path:'/guide/bbb/'}
+                    ],
+                },
+            {
+                title: 'items04',
+                collapsable: false,
+                children: [
+                    { title: 'Chinese', path:'/language/chinese/'},
+                    { title: 'English', path:'/language/english/'}
+                ]
+            }
+        ],
         sidebarDepth: 2, // 侧边栏显示2级
     }
 };
