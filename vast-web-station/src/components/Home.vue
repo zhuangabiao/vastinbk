7e06d2bcc269a98ba702eee30c3fffa610252e76
zhb <shenme2020>
2021-04-08 17:15:49 +0800

    添加web-station新模块

---
+++ vast-web-station/src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="Home">
+    {{msg}}
+    <el-container style="height: 100%; border: 1px solid #eee">
+      <el-row :gutter="20">
+        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+      </el-row>
+    </el-container>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Home",
+    data() {
+      return {
+        msg: "Hello Home"
+      }
+    }
+  }
+</script>
